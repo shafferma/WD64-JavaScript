c7862c01406b9e76d4d3276f222fa7350b3783ad
shafferma <70526699+shafferma@users.noreply.github.com>
2020-09-23 21:05:22 -0400

    project-work
    random photo - per click and name displays next to text

--- projects/API/API Challenge1/api.js
+++ projects/API/API Challenge1/api.js
@@ -1,1 +1,39 @@
-const URL = 'https://pokeapi.co/api/v2'; 
+
+const baseUrl = 'https://pokeapi.co/api/v2/pokemon/'; 
+const catchMeBtn = document.querySelector('.catch-me button');
+const pokemonImage = document.querySelector('.catch-me img')
+const pokemonName = document.querySelector('#name')
+
+// console.log(catchMeBtn);
+// console.log({pokemonImage})
+
+catchMeBtn.addEventListener('click', getAndDisplayRandomPokemon);
+
+function getAndDisplayRandomPokemon() {
+    let pokemonId = getRandomPokemonId(151)
+    console.log(pokemonId);
+
+    let url = baseUrl + pokemonId 
+    console.log(url)
+    
+    fetch(url)
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(data) {
+            // we have access to our pokemon data in here
+
+
+            // pokemon image url string
+            pokemonImage.src = data.sprites.other['official-artwork'].front_default
+            
+            console.log(data.name)
+            
+            pokemonName.textContent = data.name
+        
+        })
+}
+
+function getRandomPokemonId(maxId) {
+    return Math.floor(Math.random() * Math.floor(maxId))
+}
