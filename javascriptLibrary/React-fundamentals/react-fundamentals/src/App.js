8b68a3d3746911fd6a87d960b98988e7aeb50f41
shafferma <shafferma12@gmail.com>
2020-10-19 11:10:07 -0400

    module react

---
+++ javascriptLibrary/React-fundamentals/react-fundamentals/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.css";
+import './App.css';
+import Header from "./components/site/Header"
+import Footer from "./components/site/Footer"
+import Sidebar from "./components/site/Sidebar"
+import {
+  BrowseRouter as Router,
+} from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Router>
+        <Sidebar />
+      </Router>
+      <Footer/>
+      </div>
+  );
+}
+
+export default App;
