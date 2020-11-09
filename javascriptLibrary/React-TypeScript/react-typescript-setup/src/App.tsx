749b6a3ceb5b9817fb093cb1ff1e92ef704e0597
shafferma <shafferma12@gmail.com>
2020-11-09 10:26:07 -0500

    .

---
+++ javascriptLibrary/React-TypeScript/react-typescript-setup/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App: React.FunctionComponent = () => {
+  return (
+    <div className="App">
+      <div className="verticalCenter">
+      </div>
+    </div>
+  );
+}
+
+export default App;
