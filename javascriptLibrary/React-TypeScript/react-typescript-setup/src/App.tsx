638178d675b4c3080df65eeff4481e30997a3e59
shafferma <shafferma12@gmail.com>
2020-11-16 09:37:19 -0500

    .

--- javascriptLibrary/React-TypeScript/react-typescript-setup/src/App.tsx
+++ javascriptLibrary/React-TypeScript/react-typescript-setup/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Clock from './components/Clock';
+
+let testProp: string = 'Am I getting passed to the Clock component?'
+let optionalProp: string = 'You sure are!';
 
 const App: React.FunctionComponent = () => {
   return (
     <div className="App">
       <div className="verticalCenter">
+        <Clock testProp={testProp} optionalProp={optionalProp} />
       </div>
     </div>
   );
--- javascriptLibrary/React-TypeScript/react-typescript-setup/src/components/Clock.tsx
+++ javascriptLibrary/React-TypeScript/react-typescript-setup/src/components/Clock.tsx
@@ -1,14 +1,42 @@
 import React from 'react';
 
-class Clock extends React.Component {
-    constructor(props){
+type ClockState = {
+    time: Date
+};
+
+type AcceptedProps = {
+    testProp: string,
+    optionalProp?: string
+};
+
+class Clock extends React.Component<AcceptedProps, ClockState> {
+    constructor(props: AcceptedProps){
         super(props)
+        this.state = {
+            time: new Date()
+        }
     }
+
+    tick() {
+        this.setState({
+            time: new Date()
+        })
+    };
+
+    // componentWillMount() {
+    //     this.tick();
+    // };
+
+    componentDidMount() {
+        setInterval(() => this.tick(), 1000);
+    };
 
     render(){
         return(
             <div>
-
+                <h1>{this.state.time.toLocaleTimeString()}</h1>
+                {/* <p>{this.props.testProp}</p>
+                <p>{this.props.optionalProp}</p> */}
             </div>
         )
     }
